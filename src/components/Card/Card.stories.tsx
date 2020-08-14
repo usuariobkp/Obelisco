ce1926fb856735445fa127d39866d8ad0619c010
ghoyos95 <gonzalohoyos95@gmail.com>
2020-08-14 13:02:39 -0300

    fix

---
+++ src/components/Card/Card.stories.tsx
@@ -0,0 +1,39 @@
+// Base
+import React from "react";
+
+// Addons
+import { withA11y } from "@storybook/addon-a11y";
+
+// Configuración del general del componente
+export default {
+  // Usar | para separar entre secciones y / para generar carpetas
+  title: "Componentes|Card",
+  // Agregamos todos los plugins que usemos en las historias de este componente.
+  decorators: [withA11y]
+};
+
+/*
+ * Para cada historia vamos a exportar una constrante que devuelva un element JSX con el html.
+ * Más información en https://storybook.js.org/docs/basics/writing-stories/
+ *
+ * No hace falta que las historias usen componentes de react.
+ * pueden devolver solamente el html estático necesario
+ */
+
+// Los componentes
+
+export const Simple = (): JSX.Element => (
+  <div className="card" style={{ width: "18rem" }}>
+    <img src="..." className="card-img-top" alt="..." />
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the cards content.
+      </p>
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+    </div>
+  </div>
+);
