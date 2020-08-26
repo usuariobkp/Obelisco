fa6fc5eebd213c0f49e95246f0a12eeff54c3a81
ghoyos95 <gonzalohoyos95@gmail.com>
2020-08-26 19:28:19 -0300

    danger links

--- src/components/Alert/Alert.stories.tsx
+++ src/components/Alert/Alert.stories.tsx
@@ -29,7 +29,7 @@ export const DangerDefault = (): JSX.Element => (
   <div className="alert alert-danger" role="alert">
     <div className="row">
       <div className="col-1">
-        <img src="src/images/correct.png"></img>
+        <img src="src/images/cross.png"></img>
       </div>
       <div className="col-11">
         <p> A simple danger alert—check it out!</p>
@@ -42,7 +42,7 @@ export const DangerTittle = (): JSX.Element => (
   <div className="alert alert-danger" role="alert">
     <div className="row">
       <div className="col-1">
-        <img src="src/images/correct.png"></img>
+        <img src="src/images/cross.png"></img>
       </div>
       <div className="col-11">
         <h5> Un titulo por aqui</h5>
@@ -53,6 +53,31 @@ export const DangerTittle = (): JSX.Element => (
     </div>
   </div>
 );
+
+export const DangerLinks = (): JSX.Element => (
+  <div className="alert alert-danger" role="alert">
+    <div className="row">
+      <div className="col-1">
+        <img src="src/images/cross.png"></img>
+      </div>
+      <div className="col-11">
+        <h5> Un titulo por aqui</h5>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <a href="#">Un primer linkazo</a>
+          </li>
+          <li className="list-group-item">
+            <a href="#">Un segundo linkazo</a>
+          </li>
+          <li className="list-group-item">
+            <a href="#">Un tercer linkazo</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+);
+
 export const SuccessDefault = (): JSX.Element => (
   <div className="alert alert-success" role="alert">
     <div className="row">
